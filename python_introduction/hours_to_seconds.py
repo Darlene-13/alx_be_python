69277c54d974d4a9d161dfedd89ef35f7529825b
Darlene-13 <darlenewendie@gmail.com>
2025-07-08 16:06:43 +0300

    Initial commit

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,7 +2,7 @@
 hours = 2
 
 # Conversion factor
-seconds = hour * 3600
+seconds=hours*3600
 
 # Print the results
 print(f"{hours} hour(s) is {seconds} seconds.")
--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -2,6 +2,6 @@
 length = 10
 width = 5
 # Formula for calculating area of a rectangle
-rectangle_area = length*width
+area = length*width
 #Printing the output
-print(f"The area of the rectangle is: {rectangle_area}")
+print(f"The area of the rectangle is: {area}")
