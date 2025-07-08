8a21e0a1d9c5e1ffdf385d6d4fb6d6358740415d
Darlene-13 <darlenewendie@gmail.com>
2025-07-08 16:09:48 +0300

    Initial commit

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,15 @@
-# Definining variables
-principal = 1000
-rate = 0.05
-time = 3
+# Prompt user for income and expenses
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-#Formula for calculating simple interest
-interest = principal*rate*time
+# Calculate monthly savings
+monthly_savings = monthly_income - monthly_expenses
 
-print(f"The simple interest is: {interest}")
+# Calculate annual savings with 5% interest
+annual_savings = monthly_savings * 12
+projected_savings = annual_savings + (annual_savings * 0.05)
+
+# Display results
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
 
