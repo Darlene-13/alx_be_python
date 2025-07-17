e3a5ac899d2d3bc891336fa641fe19fb1be9e51f
Darlene-13 <darlenenasimiyu@gmail.com>
2025-07-17 14:35:09 +0300

    Initial commit

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+# Enter a positive interger and the script will draw a pattern that matches it.
+
+input = int(input("Enter the size of the pattern:"))
+
+while input >0:
+    for i in range(input):
+        print("*" * (i+1))
+        input -=1
+    print("\n")
